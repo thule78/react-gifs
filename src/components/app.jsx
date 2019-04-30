73faf778c7743c265e200d6e02e647c922aed9f9
thule <lunthu@gmail.com>
2019-04-30 15:03:51 +0700

    app, search components

---
+++ src/components/app.jsx
@@ -0,0 +1,18 @@
+import React, { Component} from 'react';
+import SearchBar from './search.jsx';
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <div className="left-scene">
+        <SearchBar />
+        </div>
+        <div className="right-scene">
+        </div>
+      </div>
+      );
+  }
+}
+
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -3,16 +3,11 @@ import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
 
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
-};
+import App from './components/app.jsx';
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
+  ReactDOM.render (
+    <App />,
+    root);
 }
